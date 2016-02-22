005fecc80f86d988b5cd75f24975c6f45a8ac1df
allosaurus <allisonhwang@uchicago.edu>
2016-02-22 10:47:52 -0600

    implemented getBestMove and Score skeleton

--- minimax.elm
+++ minimax.elm
@@ -8,6 +8,7 @@ type alias Row   = List Tile
 type alias Board = List Row
 
 getMoveFromSquare (button, T a (x,y)) = (x,y)
+npc = 2
 
 -- create tree of simulated moves
 -- ( a move is a pair of Ints representing coordinates)
@@ -33,10 +34,35 @@ simMoveTrees p h board move =
             [] -> Node move []
             _ -> Node move (List.map (\x -> simMoveTrees opponent (h-1) board' (getMoveFromSquare x)) lm) 
 -- traverse tree of simulated moves to find best move
+-- sim tree, player num, current board
+getBestMove : MovesTree -> Int -> Board -> (Int, (Int, Int))
+getBestMove mt p b = 
+  case mt of
+    -- at the leaf, we will have a different scoring algorithm; for now return 0
+    Node move [] -> (0, move)
+    Node move mts -> ((score p (List.map (\t -> getBestMove t p b) mts) b move), move)
 
 -- function to score a move
 --      have some heuristics 
 --      use heuristics to define value of leaf nodes?
+-- score : player num, board, move
+-- score : Int Int Board (Int, Int)
+score : Int -> List (Int, (Int,Int)) -> Board -> (Int, Int) -> Int
+score p children b move = 
+  let
+    getMax scorepairs = case scorepairs of 
+      -- negative infinity
+      [] -> -100
+      (score, move) :: scorepairs' -> max score (getMax scorepairs')
+    getMin scorepairs = case scorepairs of 
+      -- positive infinity
+      [] -> 100
+      (score, move) :: scorepairs' -> min score (getMin scorepairs')
+  in
+  if p == npc 
+    then getMax children 
+    else getMin children
+
 
 -- stubbed out functions from Board.elm
 boardSize = 8
